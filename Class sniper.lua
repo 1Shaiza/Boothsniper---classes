dc007287108e8ac17a507e7f12daf1036efa1a98
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2024-01-04 21:55:19 -0600

    Update Class sniper.lua

--- Class sniper.lua
+++ Class sniper.lua
@@ -1,62 +1,156 @@
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
+local http = game:GetService("HttpService")
 
-local function checklisting(uid, gems, item, version, shiny, amount, username, playerid, class)
-    gems = tonumber(gems)
-
-     if gems <= 5 then
-        task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    elseif class == "Egg" and gems <= 75000 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    elseif class == "Charm" and gems <= 10000 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    elseif class == "Enchant" and gems <= 1000 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    elseif class == "Potion" and gems <= 1000 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    elseif class == "Misc" and gems <= 1000 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom, boughtStatus, class, failMessage)
+    local gemamount = Players.LocalPlayer.leaderstats["💎 Diamonds"].Value
+    local snipeMessage ="||".. Players.LocalPlayer.Name .. "||"
+    local weburl, webContent, webcolor
+    local versionVal = { [1] = "Golden ", [2] = "Rainbow " }
+    local versionStr = versionVal[version] or (version == nil and "")
+    local mention = (string.find(item, "Huge") or string.find(item, "Titanic")) and "<@" .. userid .. ">" or ""
+	
+    if boughtStatus then
+	webcolor = tonumber(0x00ff00)
+	weburl = "https://discordapp.com/api/webhooks/1192670494487490610/AmOd7_5VXm2PXz66C3psw2Jl53Dh1IPNssoeqycMkfuVvNy0hFMThZSqziYlNXSLCsJQ"
+        snipeMessage = snipeMessage .. " just sniped a "
+        webContent = mention
+	if snipeNormal == true then
+	    weburl = normalwebhook
+	    snipeNormal = false
+	end
+    else
+	webContent = failMessage
+	webcolor = tonumber(0xff0000)
+	weburl = webhookFail
+	snipeMessage = snipeMessage .. " failed to snipe a "
+	if snipeNormal == true then
+	    weburl = "https://discordapp.com/api/webhooks/1192670625622401075/rrenhsZjp65FdKNhn0aClwcn4OvfmSLtKMx5QzUpad2dYv8GlsNBxr7AJHugHrplkdTB"
+	    snipeNormal = false
+	end
+    end
     
-    elseif class == "Fruit" and gems <= 100 then
-           task.wait(3.05)
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        
+    snipeMessage = snipeMessage .. "**" .. versionStr
+    
+    if shiny then
+        snipeMessage = snipeMessage .. " Shiny "
+    end
+    
+    snipeMessage = snipeMessage .. item .. "**"
+    
+    local message1 = {
+        ['content'] = webContent,
+        ['embeds'] = {
+            {
+		["author"] = {
+			["name"] = "Shaiza",
+			["icon_url"] = "https://cdn.discordapp.com/attachments/1034579708312027138/1192675000151457802/Snazzy.png?ex=65a9f026&is=65977b26&hm=d7714215f4dffc2f0825e6c31b5688958fae12adc9859a80e39ed8f88e627eca&",
+		},
+                ['title'] = snipeMessage,
+                ["color"] = webcolor,
+                ["timestamp"] = DateTime.now():ToIsoDate(),
+                ['fields'] = {
+                    {
+                        ['name'] = "__Price:__",
+                        ['value'] = tostring(gems) .. " 💎",
+                    },
+                    {
+                        ['name'] = "__Bought from:__",
+                        ['value'] = "||"..tostring(boughtFrom).."|| ",
+                    },
+                    {
+                        ['name'] = "__Amount:__",
+                        ['value'] = tostring(amount) .. "x",
+                    },
+                    {
+                        ['name'] = "__Remaining gems:__",
+                        ['value'] = tostring(gemamount) .. " 💎",
+                    },      
+                    {
+                        ['name'] = "__PetID:__",
+                        ['value'] = "||"..tostring(uid).."||",
+                    },
+                },
+		["footer"] = {
+                        ["icon_url"] = "https://cdn.discordapp.com/attachments/1034579708312027138/1192675124571283516/Sad.png?ex=65a9f044&is=65977b44&hm=5c952d9fd71c948e336543b3214453f3f07eee01ca43dfbfeb01672200037071&",
+                        ["text"] = "Heavily Modified by Shaiza"
+		}
+            },
+        }
+    }
+
+    local jsonMessage = http:JSONEncode(message1)
+    local success, webMessage = pcall(function()
+	http:PostAsync(weburl, jsonMessage)
+    end)
+    if success == false then
+        local response = request({
+            Url = weburl,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = jsonMessage
+        })
     end
 end
 
+local function tryPurchase(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+    if buytimestamp > listTimestamp then
+      task.wait(3.01 - Players.LocalPlayer:GetNetworkPing())
+    end
+    local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage)
+end
+
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
-    local playerID = message['PlayerID']
-    if type(message) == "table" then
-        local listing = message["Listings"]
-        for key, value in pairs(listing) do
-            if type(value) == "table" then
+        if type(message) == "table" then
+            local highestTimestamp = -math.huge -- Initialize with the smallest possible number
+            local key = nil
+            local listing = nil
+            for v, value in pairs(message["Listings"] or {}) do
+                if type(value) == "table" and value["ItemData"] and value["ItemData"]["data"] then
+                    local timestamp = value["Timestamp"]
+                    if timestamp > highestTimestamp then
+                        highestTimestamp = timestamp
+                        key = v
+                        listing = value
+                    end
+                end
+            end
+            if listing then
+                local buytimestamp = listing["ReadyTimestamp"]
+                local listTimestamp = listing["Timestamp"]
+                local data = listing["ItemData"]["data"]
+                local gems = tonumber(listing["DiamondCost"])
                 local uid = key
-                local gems = value["DiamondCost"]
-                local itemdata = value["ItemData"]
-
-                if itemdata then
-                    local data = itemdata["data"]
-
-                    if data then
-                        local item = data["id"]
-                        local version = data["pt"]
-                        local shiny = data["sh"]
-                        local amount = data["_am"]
-                        local class = itemdata['class']
-                        checklisting(uid, gems, item, version, shiny, amount, username , playerID, class)
+                local item = data["id"]
+                local version = data["pt"]
+                local shiny = data["sh"]
+                local amount = tonumber(data["_am"]) or 1
+                local playerid = message['PlayerID']
+                local class = tostring(listing["ItemData"]["class"])
+                local unitGems = gems/amount
+                                 
+                if class == "Egg" and unitGems <= 75000 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
+ elseif class == "Charm" and unitGems <= 10000 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
+ elseif class == "Enchant" and unitGems <= 1000 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
+ elseif class == "Potion" and unitGems <= 1000 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
+ elseif class == "Misc" and unitGems <= 1000 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
+ elseif class == "Fruit" and unitGems <= 100 then
+           coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
+                    return
                     end
                 end
             end
         end
-    end
-end)
+    end)
