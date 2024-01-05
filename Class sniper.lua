03a4be76fedc142250d277634066dec8d0741281
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2024-01-05 09:51:17 -0600

    Update Class sniper.lua

--- Class sniper.lua
+++ Class sniper.lua
@@ -1,9 +1,9 @@
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 
-local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
+local function checklisting(uid, gems, item, version, shiny, amount, username, playerid, class)
     gems = tonumber(gems)
 
-    if gems <= 5 then
+     if gems <= 5 then
         task.wait(3.05)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
@@ -30,5 +30,33 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     elseif class == "Fruit" and gems <= 100 then
            task.wait(3.05)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        
     end
 end
+
+Booths_Broadcast.OnClientEvent:Connect(function(username, message)
+    local playerID = message['PlayerID']
+    if type(message) == "table" then
+        local listing = message["Listings"]
+        for key, value in pairs(listing) do
+            if type(value) == "table" then
+                local uid = key
+                local gems = value["DiamondCost"]
+                local itemdata = value["ItemData"]
+
+                if itemdata then
+                    local data = itemdata["data"]
+
+                    if data then
+                        local item = data["id"]
+                        local version = data["pt"]
+                        local shiny = data["sh"]
+                        local amount = data["_am"]
+                        local class = itemdata['class']
+                        checklisting(uid, gems, item, version, shiny, amount, username , playerID, class)
+                    end
+                end
+            end
+        end
+    end
+end)
