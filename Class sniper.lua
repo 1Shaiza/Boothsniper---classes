04174aa6bd247d34e4580850bee317cf11c9042b
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2024-01-05 09:59:43 -0600

    changed time

--- Class sniper.lua
+++ Class sniper.lua
@@ -4,31 +4,31 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     gems = tonumber(gems)
 
      if gems <= 5 then
-        task.wait(3.05)
+        task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
     elseif class == "Egg" and gems <= 75000 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
     elseif class == "Charm" and gems <= 10000 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
     elseif class == "Enchant" and gems <= 1000 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
     elseif class == "Potion" and gems <= 1000 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
 
     elseif class == "Misc" and gems <= 1000 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
     
     elseif class == "Fruit" and gems <= 100 then
-           task.wait(3.05)
+           task.wait(3.01)
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         
     end
